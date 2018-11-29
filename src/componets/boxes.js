959d06c08af658a980dce14295cf49a5d69fcd20
Shayan Mahmoudi <iamsha4yan@gmail.com>
2018-11-30 02:33:59 +0330

    Box Component

---
+++ src/componets/boxes.js
@@ -0,0 +1,58 @@
+import React, { Component } from 'react';
+import '../styles/App.css';
+
+class BOXES extends Component {
+  render() {
+    return (
+      <div>
+          <a class="direct-access1">
+              <img src="picture/main1.png">
+          </a>
+
+          <a class="direct-access2">
+              <img src="picture/main2.jpg">
+          </a>
+
+          <a class="direct-access3">
+              <img src="picture/main3.png">
+          </a>
+
+          <a class="direct-access4">
+              <img src="picture/main4.jpg">
+          </a>
+
+          <a class="direct-access1">
+              <img src="picture/main5.jpg">
+          </a>
+
+          <a class="direct-access2">
+              <img src="picture/main6.jpg">
+          </a>
+
+          <a class="direct-access3">
+              <img src="picture/main7.jpg">
+          </a>
+
+          <a class="direct-access4">
+              <img src="picture/main8.jpg">
+          </a>
+          <a class="direct-access1">
+              <img src="picture/main9.jpg">
+          </a>
+
+          <a class="direct-access2">
+              <img src="picture/main10.jpg">
+          </a>
+
+          <a class="direct-access3">
+              <img src="picture/main11.jpg">
+          </a>
+
+          <a class="direct-access4">
+              <img src="picture/main12.jpg">
+          </a>
+
+      </div>
+    );
+  }
+}
